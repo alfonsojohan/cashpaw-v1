0eb0d8cdcada4f9fe9f0fdc05b62bf1aad131573
A Johan <ajohan75@gmail.com>
2016-06-01 10:08:20 +0800

    Sync to current cashpaw app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,107 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function ($scope) { })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  .controller('TasksCtrl', function ($scope, $state, $stateParams, $ionicHistory, Chats, Chores) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+    console.log('in TasksCtrl', $stateParams);
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    this.headerTitle = 'New Task';
+    this.task = {};
+    this.originalTask = null;
+    this.edit = false;
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    this.labels = {
+      duration: 'How long?',
+      points: 0,
+      category: 'Nothing here...'
+    };
+
+    this.chores = null;
+    
+    this.task = {
+      category: "Nothing here yet...",
+      categoryImg: "img/smiley-flat.png",
+      duration: 0,
+      id: null,
+      img: "img/smiley-flat.png",
+      name: null,
+      note: null,
+      points: 0,
+    };
+
+    if ($state.is('tab.tasks')) {
+      //  this.chats = Chats.all();
+      this.chores = Chores.all();
+
+    } else if ($state.is('tab.new-task')) {
+
+    } else if ($state.is('tab.edit-task')) {
+
+      this.edit = true;
+      this.headerTitle = 'Edit Task';
+
+      /**
+       * Operate on a copy of the object until the user clicks on save
+       */
+      this.task = Chores.get($stateParams.taskId);
+      this.originalTask = angular.copy(this.task);
+
+      // this.labels.points = this.task.points;
+      // this.labels.duration = this.task.duration + ' mins';
+      // this.labels.category = this.task.category;
+
+      // this.task = Chats.get($stateParams.chatId);
+      // this.originalTask = angular.copy(this.task);
+    }
+
+    this.remove = function (chat) {
+      console.log('in TasksCtrl.remove');
+      Chores.remove(chat);
+    };
+
+    this.removeFromDetailView = function (task) {
+      console.log('in TasksCtrl.removeFromDetailView');
+
+      this.remove(task);
+
+      $state.go('tab.tasks', null, {
+        location: 'replace'
+      });
+    };
+
+    this.onCheckChange = function (id) {
+      console.log('in onCheckChange. Id: ', id);
+    };
+
+    this.newTask = function () {
+      console.log('in TasksCtrl.newTask');
+
+      $state.go('tab.new-task');
+    };
+
+    this.cancel = function () {
+
+      // Restore the task object
+      angular.copy(this.originalTask, this.task);
+
+      // Go back to main tasks view and do not keep in history
+      $state.go('tab.tasks', {}, {
+        location: "replace"
+      });
+    };
+
+    this.saveTask = function () {
+      console.log('in NewTaskCtrl.saveTask');
+
+      $state.go('tab.tasks', {}, {
+        location: "replace"
+      });
+    };
+  })
+
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+  ;
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,167 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  .factory('Chores', function () {
+    // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    var choreList = ['Do homework', 'Clean room', 'Throw garbage', 'Vacuum room', 'Laundry'];
+    var categoryList = ['House', 'Outdoors', 'Meals', 'Learning', 'General', 'Cleaning'];
+    var descList = [
+      'You need to do this!',
+      'Eat something',
+      'Please get it done',
+      'No pain no gain',
+      'Disneyworld is not a dream',
+      'Its pasta night tonight'
+    ];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    var imgList = [
+      'adam.jpg',
+      'ben.png',
+      'max.png',
+      'mike.png',
+      'perry.png',
+    ];
+
+    var chores = [];
+    var choreCount = 10;
+
+    /**
+     * Generate random data
+     */
+    for (var i = 0, r = 0; i < choreCount; i++) {
+
+      chore = { id: i };
+
+      r = Math.floor((Math.random() * choreList.length));
+      chore.name = choreList[r];
+
+      r = Math.floor((Math.random() * categoryList.length));
+      chore.category = categoryList[r];
+      chore.categoryImg = 'img/categories/price-tag.png';
+      
+      r = Math.floor((Math.random() * categoryList.length));
+      chore.note = descList[r];
+
+      r = Math.floor((Math.random() * imgList.length));
+      chore.img = 'img/' + imgList[r];
+
+      r = Math.round(Math.random() * (200 - 10) + 10);
+      chore.points = Math.round(r / 10) * 10;
+
+      r = Math.round(Math.random() * (120 - 15) + 15);
+      r = Math.ceil(r/5) * 5;
+      chore.duration = r;
+      
+      chore.completed = false;
+
+      chores.push(chore);
+    };
+
+    console.log('Chores', chores);
+
+    return {
+      all: function () {
+        return chores;
+      },
+      remove: function (chore) {
+        chores.splice(chores.indexOf(chore), 1);
+      },
+      get: function (choreId) {
+        for (var i = 0; i < chores.length; i++) {
+          if (chores[i].id === parseInt(choreId)) {
+            return chores[i];
+          }
         }
+        return null;
       }
-      return null;
-    }
-  };
-});
+    };
+  })
+
+  .factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var chats = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'img/ben.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'img/max.png'
+      }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'img/adam.jpg'
+      }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+      }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+      }, {
+        id: 5,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+      }, {
+        id: 6,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+      }, {
+        id: 7,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+      }, {
+        id: 8,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+      }, {
+        id: 9,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+      }, {
+        id: 10,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+      }, {
+        id: 11,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+      }, {
+        id: 12,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+      }];
+
+    return {
+      all: function () {
+        return chats;
+      },
+      remove: function (chat) {
+        chats.splice(chats.indexOf(chat), 1);
+      },
+      get: function (chatId) {
+        for (var i = 0; i < chats.length; i++) {
+          if (chats[i].id === parseInt(chatId)) {
+            return chats[i];
+          }
+        }
+        return null;
+      }
+    };
+  });
